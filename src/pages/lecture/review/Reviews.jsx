11389b56a19aebc251047fbd380b16aa603392ab
CallmeEterHist <kjls515@gmail.com>
2026-04-15 12:55:52 +0900

    [FEATURE] 리뷰 페이지 제작 중

--- src/pages/lecture/review/Reviews.jsx
+++ src/pages/lecture/review/Reviews.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Reviews.css'
+import { useParams } from 'react-router-dom'
+import leftArrow from '../../../assets/leftArrow.svg'
 
 function Reviews() {
+    const lectureId = useParams().id;
+    const lecture = {
+        id: 123,
+        title: ''
+    }
     return (
-        <div>
-
+        <div className='review-layout'>
+            <div className='review-back'>
+                <img src={leftArrow} width="20" height="20" />
+                <p>강의 상세보기로 돌아가기</p>
+            </div>
+            <div className='review-head'>
+                <h2>수강생 후기</h2>
+                <p></p>
+            </div>
+            <div className='review-dashboard'></div>
+            <div className='review-filter'></div>
+            <div className='review-list'></div>
+            <div className='review-page'></div>
         </div>
     )
 }

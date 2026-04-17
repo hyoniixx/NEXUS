8c74933cb150673784ad78ca0f348dfa403d3de2
CallmeEterHist <kjls515@gmail.com>
2026-04-17 12:59:52 +0900

    [FEATURE] 관리자 회원관리 페이지 완성

--- src/components/admin/MemberListItem.jsx
+++ src/components/admin/MemberListItem.jsx
@@ -1,36 +1,65 @@
 import React from 'react'
 import './MemberListItem.css'
+import pro from '../../assets/memberProbadge.svg'
+import strm from '../../assets/memberStrmbadge.svg'
+import scoreBadge from '../../assets/memberScorebadge.svg'
+import blackBadge from '../../assets/memberBlack.svg'
+import whiteBadge from '../../assets/memberWhite.svg'
 
-function MemberListItem({ name, id, role, email, date, score, isblack }) {
+function MemberListItem({ name, id, role, isPro = 'false', isStrm = 'false', email, date, score, isblack }) {
+    const roleText = role === 'student' ? '수강생' : role === 'instructor' ? '강사' : role === 'admin' ? '관리자' : '알 수 없음';
+    const roleColor = role === 'student' ? '#8B5CF6' : role === 'instructor' ? '#3B82F6' : role === 'admin' ? '#d034ef' : '#e52d2d';
+    const buttonColor = isblack === 'false' ? ['rgba(239, 68, 68, 0.10)', '#EF4444'] : ['rgba(0, 201, 80, 0.10)', '#00C950'];
     return (
         <div className='member-list-item'>
             <div className='member-list-item-profile'>
-                <img src="" alt="" width="48px" height="48px" style={{ border: '1px solid black' }} />
+                <img src="" width="52px" height="52px" style={{ border: '1px solid black' }} />
                 <div className='member-list-item-name'>
                     <h6>{name}</h6>
+                    <div className='member-list-badge'>
+                        <div
+                            className='member-list-probadge'
+                            style={{ display: isPro == 'false' ? 'none' : 'flex' }}
+                        >
+                            <img src={pro} width="12" height="12" />
+                            <p>PRO</p>
+                        </div>
+                        <div
+                            className='member-list-strmbadge'
+                            style={{ display: isStrm == 'false' ? 'none' : 'flex' }}
+                        >
+                            <img src={strm} width="12" height="12" />
+                            <p>스트리머</p>
+                        </div>
+                    </div>
                     <p>{id}</p>
                 </div>
             </div>
             <div className='member-list-item-info'>
                 <div className='member-list-item-role'>
                     <h6>직책 : </h6>
-                    <p>{role}</p>
+                    <p style={{ color: roleColor }}>{roleText}</p>
                 </div>
                 <div className='member-list-item-email'>
                     <h6>이메일 : </h6>
                     <p>{email}</p>
                 </div>
             </div>
-            <div className='member-list-item-date'>
-                <h6>가입일 : </h6>
-                <p>{date}</p>
-            </div>
-            <div className='member-list-item-footer'>
-                <div className='member-list-item-score'>
-                    <img src="" alt="" />
-                </div>
-                <div className='member-list-black-button'>
 
+            <div className='member-list-item-footer'>
+                <div className='member-list-item-date'>
+                    <h6>가입일 : </h6>
+                    <p>{date}</p>
+                </div>
+                <div className='member-list-item-manage'>
+                    <div className='member-list-item-score'>
+                        <img src={scoreBadge} width="32" height="32" />
+                        <p>{score}점</p>
+                    </div>
+                    <button className='member-list-black-button' style={{ background: buttonColor[0] }}>
+                        <img src={isblack == 'true' ? whiteBadge : blackBadge} width="16" height="16" />
+                        <p style={{ color: buttonColor[1] }}>{isblack == 'false' ? '차단' : '해제'}</p>
+                    </button>
                 </div>
             </div>
         </div>
--- src/pages/admin/Members.jsx
+++ src/pages/admin/Members.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import './Members.css'
 import leftArrow from '../../assets/leftArrow.svg'
 import search from '../../assets/searchIcon.svg'
 import right from '../../assets/rightIcon.svg'
+import MemberListItem from '../../components/admin/MemberListItem'
 
 function Members() {
     const [navSelected, setNavSelected] = useState([true, false, false, false]);
     const [onlyBlacklist, setOnlyBlacklist] = useState(true);
-    const total = 25; //전체 데이터 수
+    const total = 27; //전체 데이터 수
     const [pageNow, setPageNow] = useState(1); //현재 선택된 페이지
-    const [pages, setPages] = useState(100); //전체 페이지수
+    const [pages, setPages] = useState(1); //전체 페이지수
     const [pagination, setPagination] = useState([1, 0, 0, 0, 0]); //페이지네이션
     const [pageSelected, setPageSelected] = useState([true, false, false, false, false]) //페이지 아이템들 상태 관리(눌려있는지)
     const [canClick, setCanClick] = useState([false, true]) //좌우 이동 버튼 활성화 관리
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const tempPage = Math.ceil(total / 5); //전체 페이지 수 지정(올림)
@@ -51,7 +54,7 @@ function Members() {
         if (pageNow <= 5) {
             leftTemp = false
         } else { leftTemp = true }
-        if ((pages > pageNow - ((pageNow - 1) % 5) + 5)) {
+        if (pageNow < pages) {
             rightTemp = true;
         } else { rightTemp = false; }
         setCanClick([leftTemp, rightTemp]);
@@ -60,7 +63,10 @@ function Members() {
     return (
         <div className='admin-members-layout'>
             <div className='admin-members-header'>
-                <div className='admin-members-header-back'>
+                <div
+                    className='admin-members-header-back'
+                    onClick={() => navigate('/mypage')}
+                >
                     <img src={leftArrow} alt="" style={{ width: '20px', height: '20px' }} />
                     <p>마이페이지로 돌아가기</p>
                 </div>
@@ -130,9 +136,14 @@ function Members() {
                 >
                     블랙리스트만 보기
                 </button>
+                <p>{Number(pageNow) * 5 - 4}-{Math.min(Number(pageNow) * 5, total)} / {total}명</p>
             </div>
             <div className='admin-members-list'>
-                컴포넌트*5
+                <MemberListItem name='김태정' id='user01' role='student' email='user01@nexus.com' date='2025.04.01' score='93' isblack='false' />
+                <MemberListItem name='김블랙' id='user02' role='student' email='user02@nexus.com' date='2025.04.04' score='11' isblack='true' />
+                <MemberListItem name='이지경' id='user03' isStrm='true' role='instructor' email='inst99@nexus.com' date='2022.02.11' score='153' isblack='false' />
+                <MemberListItem name='대상혁' id='user04' isPro='true' isStrm='true' role='instructor' email='faker@nexus.com' date='2016.04.01' score='939' isblack='false' />
+                <MemberListItem name='블랙됨' id='user05' role='student' email='ban1919@nexus.com' date='2022.03.28' score='3' isblack='true' />
             </div>
             <div className='members-pagination'>
                 <button className='members-page-button' onClick={() => handlePageNow(pagination[0] - 5)} disabled={!canClick[0]}><img src={right} alt="" width='5px' height='10px' style={{ transform: 'scaleX(-1)' }} /></button>

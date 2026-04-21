b9d7a234f7fa05c65b385b034da04b0d59e6e85f
CallmeEterHist <kjls515@gmail.com>
2026-04-21 14:37:50 +0900

    [FEATURE] 강사승인모달 연결

--- src/components/common/TwoButtonModal.jsx
+++ src/components/common/TwoButtonModal.jsx
@@ -1,57 +1,41 @@
 import React, { useContext } from 'react'
-import { userContext } from '../../App';
 import './TiersModal.css';
-import tier1 from '../../assets/tier1.png'
-import tier2 from '../../assets/tier2.png'
-import tier3 from '../../assets/tier3.png'
-import tier4 from '../../assets/tier4.png'
-import tier5 from '../../assets/tier5.png'
-import tier6 from '../../assets/tier6.png'
-import tier7 from '../../assets/tier7.png'
-import tier8 from '../../assets/tier8.png'
-import tier9 from '../../assets/tier9.png'
-import tier from '../../assets/tier.svg'
+import icon from '../../assets/approve.png'
+import tier from '../../assets/pleaseApprove.png'
 
-function TwoButtonModal({ setIsModal }) {
-    const { userData } = useContext(userContext);
 
-    const closeModal = () => {
+function TwoButtonModal({ setIsModal, isModal, handleApproval, setOneButtonModal }) {
+
+    const deny = () => { //그냥 닫히게
         setIsModal(false);
     }
 
-    const TIER_IMAGES = {
-        1: { img: tier1, tier: '미니언' },
-        2: { img: tier2, tier: '탱크 미니언' },
-        3: { img: tier3, tier: '바위게' },
-        4: { img: tier4, tier: '돌거북' },
-        5: { img: tier5, tier: '레드' },
-        6: { img: tier6, tier: '드래곤' },
-        7: { img: tier7, tier: '전령' },
-        8: { img: tier8, tier: '바론' },
-        9: { img: tier9, tier: '장로 드래곤' },
-    };
+    const accept = () => { //닫히면서 db건드리게
+        setIsModal(false);
+        handleApproval();
+        setOneButtonModal(true);
+    }
 
 
-
+    if (!isModal) return;
 
 
     return (
-        <section className="c-modal-background" onClick={closeModal} >
+        <section className="c-modal-background" onClick={deny} >
             <div className="c-tier-modal-ct" onClick={(e) => e.stopPropagation()}>
                 {/* 제목쪽 */}
                 <article className='c-tiermodal-top'>
-                    <div className='c-tiermodal-title'>
-                        <img src={tier} /><h4>CS 등급표</h4>
+                    <div className='c-tiermodal-title' style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <img src={icon} width='20px' height='20px' style={{ marginRight: '20px' }} /><h4>강사 승인</h4>
                     </div>
-                    <p>현재 등급: <span>{TIER_IMAGES[userData.csGrade].tier}</span> {`(${userData.csScore}점)`}</p>
                 </article>
                 <article className='c-tiermodal-middle'>
-                    내용 들어갈 거
+                    <img src={tier} alt="asd" width='100%' height='100%' />
                 </article>
                 {/* 나가기 버튼 */}
                 <article className='c-tiermodal-bottom'>
-                    <button id='c-red-btn' onClick={closeModal}>확인</button>
-                    <button onClick={closeModal}>확인</button>
+                    <button id='c-red-btn' onClick={deny}>닫기</button>
+                    <button onClick={accept}>승인</button>
                 </article>
             </div>
         </section>
